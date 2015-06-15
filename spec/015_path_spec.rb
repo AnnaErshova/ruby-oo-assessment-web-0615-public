ab329307c00a3f9f562ad4fbcac102c21ea1317d
AnnaErshova <anna.ershova@gmail.com>
2015-06-15 17:56:22 -0400

    adds draft of a test suite

--- spec/015_path_spec.rb
+++ spec/015_path_spec.rb
@@ -1,4 +1,25 @@
-require_relative "../lib/path.rb" # Code your solution here.
+require_relative "../lib/path.rb" 
+
+describe Path do 
+
+  let(:relative_path) {Path.new('../lib/path.rb')}
+  let(:absolute_path) {Path.new('/lib/path.rb')}
+
+  describe('#normalize_path') do
+    it 'return an absolute path to the file no matter what kind of path sent to it'
+      do normalized_path = relative_path.normalize_path
+        expect(normalized_path) to start_with('/')
+      end
+  end
+
+
+  describe '#relative_path' do # denotes instance method
+  end
+
+  describe '#absolute_path?' do # denotes instance method
+  end
+
+end
 
 # Code some tests for path.rb.
 
